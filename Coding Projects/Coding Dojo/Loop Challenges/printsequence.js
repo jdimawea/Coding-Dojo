0117599ba93f586f7804ac5ff9ba976750afe1ac
Jonathan Dima <dimawea@gmail.com>
2021-12-21 16:20:07 -0700

    final

---
+++ Coding Projects/Coding Dojo/Loop Challenges/printsequence.js
@@ -0,0 +1,15 @@
+for(var i=5; i > -4; i -= .5) {
+    if(i == 4) {
+        console.log(i);
+    }   else if(i == 2.5) {
+            console.log(i);
+        }   else if(i == 1) {
+                console.log(i);
+            }   else if(i == -0.5) {
+                    console.log(i);
+                }   else if(i == -2) {
+                        console.log(i);
+                    }   else if(i == -3.5) {
+                            console.log(i);
+                        }
+}
---
+++ Coding Projects/Coding Dojo/Loop Challenges/sigma.js
@@ -0,0 +1,5 @@
+var x = 0
+for(i = 1; i <= 100; i++) {
+    x += i;
+}
+console.log(x);
